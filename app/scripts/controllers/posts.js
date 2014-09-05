74c2127f10470f97ecbee16eb9dad8405493d70e
Victoria de Winter <dewinter.victoria@gmail.com>
2014-09-05 16:01:52 -0700

    Added nav

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -4,13 +4,6 @@ app.controller('PostsCtrl', function ($scope, $location, Post) {
     $scope.posts = Post.all;
     $scope.post = {url: 'http://'};
 
-    $scope.submitPost = function() {
-        Post.create($scope.post).then(function(ref) {
-            // redirect to posts page
-            $location.path('/posts' + ref.name());
-        });
-    };
-
     $scope.deletePost = function(post) {
        Post.delete(post);
     };
